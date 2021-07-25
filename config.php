d98f2d3ef7559c18ef662a96630d8ab1bb859ed5
Alex Yanushevski <Yasha85@gmail.com>
2021-07-25 20:10:56 +0300

    revision1

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
    $host = 'localhost'; //имя хоста, на локальном компьютере это localhost
-$user = 'mysql'; //имя пользователя, по умолчанию это root
-$password = 'mysql'; //пароль, по умолчанию пустой
-$db_name = 'storeBooks'; //имя базы данных
+$user = 'z292401s_1'; //имя пользователя, по умолчанию это root
+$password = 'xNoc5&yy'; //пароль, по умолчанию пустой
+$db_name = 'z292401s_1'; //имя базы данных
 //Соединяемся с базой данных используя наши доступы:
 $link = mysqli_connect($host, $user, $password, $db_name);
 
--- parts/footer.php
+++ parts/footer.php
@@ -1,3 +1,3 @@
 <footer class="container">
- <p>&copy; Издательство "New Book" <?echo "2017 - ", date(Y);?></p>
+ <p>&copy; Издательство "New Book" <?= "2017 - ", date("Y");?></p>
 </footer>
