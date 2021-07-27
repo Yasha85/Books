933829644026da865b10c4c8e24d22768b02cfc8
Alex Yanushevski <Yasha85@gmail.com>
2021-07-27 22:37:21 +0300

    revision1.1

--- parts/header.php
+++ parts/header.php
@@ -1,12 +1,12 @@
 <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 bg-white">
-   <h5 class="my-0 mr-md-auto font-weight-normal">Издательство "New Book"</h5>
-   <nav class="my-2 my-md-0 mr-md-3">
-      <a class="p-2 text-dark" href="/?page=1">Главная</a>
-      <a class="p-2 text-dark" href="/authors">Авторы<a/>
-         <a class="p-2 text-dark" href="/genres">Жанры</a>
-         <a class="p-2 text-dark" href="#">Годы издания</a>
-         <a class="p-2 text-dark" href="#">Контакты</a>
-         <a class="p-2 text-dark" href="writeToUs">Напишите нам</a>
-      </nav>
-   </div>
+    <h5 class="my-0 mr-md-auto font-weight-normal">Издательство "New Book"</h5>
+    <nav class="my-2 my-md-0 mr-md-3">
+        <a class="p-2 text-dark" href="/?page=1">Главная</a>
+        <a class="p-2 text-dark" href="/authors">Авторы</a>
+        <a class="p-2 text-dark" href="/genres">Жанры</a>
+        <a class="p-2 text-dark" href="#">Годы издания</a>
+        <a class="p-2 text-dark" href="#">Контакты</a>
+        <a class="p-2 text-dark" href="writeToUs">Напишите нам</a>
+    </nav>
+</div>
 
